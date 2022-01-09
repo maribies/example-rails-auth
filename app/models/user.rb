24de47c1c59c73258c0eaa7901b77b7d9d3c955b
Marissa Biescker <marissa.biesecker@gmail.com>
2022-01-09 01:38:02 +0000

    A fix for password validation on changing password
    Co-authored-by Josh Cheek <josh.cheek@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
          :validatable,
          :trackable,
          :magic_link_authenticatable
+
+  def password_required?
+    # Method from active record
+    # Overriding this: https://github.com/abevoelker/devise-passwordless/blob/cb35253fd3cf58a75c087503ff41bea316117b64/lib/devise/models/magic_link_authenticatable.rb#L8
+    will_save_change_to_encrypted_password?
+  end
 end
